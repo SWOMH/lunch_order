4540c5368e9c29bac89df818b3190f71a7486022
SWOMH <tifame67@mail.ru>
2025-03-21 22:23:26 +0300

    upd database models

--- database/models/lunch.py
+++ database/models/lunch.py
@@ -1,51 +1,54 @@
-from sqlalchemy import ForeignKey, Time, Column, Integer, Float, String, Date, Boolean, DateTime
-from ..base import Base
+from sqlalchemy import ForeignKey, Column, Integer, Float, String, Boolean, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
+
+from ..base import Base
 
 # TABLE REVENUE =============================================>
 
 
 class Dish(Base):
     __tablename__ = 'dish'
-    id = Column(Integer, primary_key=True)
-    dish_name = Column(String, nullable=False)  # Блюдо
-    description = Column(String)  # Описание
-    price = Column(Float, nullable=True)  # Цена
-    available = Column(Boolean, default=True, nullable=False)  # Достпно для заказа
-    image = Column(String)  # Картинка блюда
-    type = Column(String, nullable=False)  # Тип блюда
-    stop_list = Column(Boolean, default=False)  # на стопе ли блюдо?
+    id: Mapped[int] = mapped_column(primary_key=True)
+    _id: Mapped[str] = mapped_column(nullable=True)  # Сервисный id блюда
+    dish_name: Mapped[str] = mapped_column(nullable=False)  # Блюдо
+    description: Mapped[str]  # Описание
+    price: Mapped[float] = mapped_column(nullable=True)  # Цена
+    available: Mapped[bool] = mapped_column(default=True, nullable=False)  # Доступно для заказа
+    image: Mapped[str]  # Картинка блюда
+    type: Mapped[str] = mapped_column(nullable=False)  # Тип блюда
+    stop_list: Mapped[bool] = mapped_column(default=False)  # На стопе ли блюдо?
 
 
 class DishVariant(Base):
     __tablename__ = 'dish_variant'
-    id = Column(Integer, primary_key=True)
-    dish_id = Column(Integer, ForeignKey('public.dish.id', ondelete='CASCADE'), nullable=False)
-    size = Column(String, nullable=False)  # Размер/вариант блюда (например, "Маленький", "Большой")
-    price = Column(Float, nullable=False)  # Цена для этого варианта
+    id: Mapped[int] = mapped_column(primary_key=True)
+    dish_id: Mapped[int] = mapped_column(ForeignKey('public.dish.id', ondelete='CASCADE'), nullable=False)
+    size: Mapped[str] = mapped_column(nullable=False)  # Размер/вариант блюда (например, "Маленький", "Большой")
+    price: Mapped[float] = mapped_column(nullable=False)  # Цена для этого варианта
 
 
 class User(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer)
-    telegram_name = Column(String)
-    telegram_username = Column(String)
-    full_name = Column(String, nullable=False)
-    is_support = Column(Boolean, default=False)
-    is_admin = Column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    telegram_id: Mapped[int]  # ID пользователя в Telegram
+    telegram_name: Mapped[str] = mapped_column(nullable=True)  # Имя пользователя в Telegram
+    telegram_username: Mapped[str] = mapped_column(nullable=True)  # Юзернейм в Telegram
+    full_name: Mapped[str] = mapped_column(nullable=False)  # Полное имя пользователя
+    is_support: Mapped[bool] = mapped_column(default=False)  # Является ли пользователь поддержкой
+    is_admin: Mapped[bool] = mapped_column(default=False)  # Является ли пользователь администратором
 
 
 class Order(Base):
     __tablename__ = 'orders'
-    id = Column(Integer, primary_key=True)  # Уникальный номер заказа
-    user_id = Column(Integer, ForeignKey('public.user.id'), nullable=False)  # Кто сделал заказ
-    datetime = Column(DateTime, default=datetime.now())  # Когда сделан заказ
+    id: Mapped[int] = mapped_column(primary_key=True)  # Уникальный номер заказа
+    user_id: Mapped[int] = mapped_column(ForeignKey('public.user.id'), nullable=False)  # Кто сделал заказ
+    datetime: Mapped[datetime] = mapped_column(default=datetime.now())  # Когда сделан заказ
 
 
 class OrderItem(Base):
     __tablename__ = 'order_items'
-    id = Column(Integer, primary_key=True)
-    order_id = Column(Integer, ForeignKey('public.orders.id'))  # Ссылка на заказ
-    dish_id = Column(Integer, ForeignKey('public.dish.id'))  # Ссылка на блюдо
-    quantity = Column(Integer, default=1)  # Количество блюд
+    id: Mapped[int] = mapped_column(primary_key=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey('public.orders.id'))  # Ссылка на заказ
+    dish_id: Mapped[int] = mapped_column(ForeignKey('public.dish.id'))  # Ссылка на блюдо
+    quantity: Mapped[int] = mapped_column(default=1)  # Количество блюд
