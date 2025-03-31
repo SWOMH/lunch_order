2a7f3211d74a162f7c6c8124967b7374ae4aa8d8
SWOMH <tifame67@mail.ru>
2025-03-31 23:07:06 +0300

    upd and add types

--- custom_types.py
+++ custom_types.py
@@ -1,8 +1,23 @@
 from pydantic import BaseModel, Field
+from typing import Optional
+
+class TelegramId(BaseModel):
+    telegram_id: int = Field(ge=0, description="ID пользователя в Telegram")  # Не до конца уверен, что в телеге id не с - идут. Потом проверю
+
+class UserSchema(TelegramId):
+    full_name: str = Field(max_length=100)    
+
+class DishOrder(BaseModel):
+    dish_id: int = Field(gt=0, description="ID блюда в меню")
+    count: int = Field(gt=0, description="Количество единиц блюда")
+    variant_id: Optional[int] = Field(
+        None, gt=0, description="ID варианта блюда (например, размер или добавка)"
+    )
 
 
-class UserSchema(BaseModel):
-    full_name: str
-    telegram_id: int = Field(ge=0)  # Не до конца уверен, что в телеге id не с - идут. Потом проверю
-
+class OrderType(TelegramId):
+    dishes: list[DishOrder] = Field(description="Список заказанных блюд")
+    amount: float = Field(
+        ge=0, description="Общая сумма заказа в рублях"
+    )
 
