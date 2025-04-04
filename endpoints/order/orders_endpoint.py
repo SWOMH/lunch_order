04bba26088061d6ccafe8c8ccea655acff97f94b
SWOMH <tifame67@mail.ru>
2025-04-05 00:51:40 +0300

    add endpoint getting actual orders

--- endpoints/order/orders_endpoint.py
+++ endpoints/order/orders_endpoint.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from custom_types import OrderType
+from custom_types import OrderType, TelegramId
 from database.bloc import database_order
 
 router = APIRouter(
@@ -18,6 +18,19 @@ async def ordering_food(order: OrderType):
         raise HTTPException(
             status_code=500,
             detail=f"Ошибка при создании заказа: {str(e)}"
+        )
+    
+@router.post("/actual_orders", tags=["order"])
+async def get_actual_orders(telegram_id: TelegramId):
+    try:
+        result = await database_order.get_actual_orders(telegram_id)
+        return result
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Ошибка: {str(e)}"
         )
     
     # {
