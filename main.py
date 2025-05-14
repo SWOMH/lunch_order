ed4fa5d25214d7b5d14f4c6606e3a6aab4290571
SWOMH <tifame67@mail.ru>
2025-05-14 23:23:49 +0300

    prod version

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from endpoints.dish.dishes_endpoint import router as dish_router
 from endpoints.user.user_endpoint import router as user_router
 from endpoints.order.orders_endpoint import router as order_router
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-app.include_router(dish_router)
-app.include_router(user_router)
-app.include_router(order_router)
+app = FastAPI(root_path="/api")
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    response = await call_next(request)
+    response.headers["X-Forwarded-Proto"] = "https"
+    return response
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Разрешенный origin
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=["*"],  # Разрешить все методы (GET, POST, PUT и т. д.)
-    allow_headers=["*"],  # Разрешить все заголовки
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
+app.include_router(dish_router)
+app.include_router(user_router)
+app.include_router(order_router)
